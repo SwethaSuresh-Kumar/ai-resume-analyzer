73a10e97dc47c10103bd5c7ae57cd740082400c1
SwethaSuresh-Kumar <swethasureshkumar.sk@gmail.com>
2025-10-01 14:38:14 +0200

    authentication

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Link} from "react-router";
+import { usePuterStore } from '~/lib/puter';
+
+const Navbar = () => {
+     const { auth } = usePuterStore();
+    return (
+        <nav className="navbar">
+            <Link to="/">
+                <p className="text-2xl font-bold text-gradient">RESUMARK</p>
+            </Link>
+            <Link to="/upload" className="primary-button w-fit">
+                Upload Resume
+            </Link>
+            <button onClick={auth.signOut}>Log out</button>
+        </nav>
+    );
+};
+
+export default Navbar;
